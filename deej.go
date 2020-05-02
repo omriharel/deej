03aaf421a4a6982db98a74faa237b1790fd2a7f9
omriharel <omriharel@users.noreply.github.com>
2020-05-03 02:10:54 +0300

    notify and quit if initial serial connection fails due to busy or wrong com port

--- deej.go
+++ deej.go
@@ -3,6 +3,7 @@
 package deej
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -121,6 +122,27 @@ func (d *Deej) run() {
 	go func() {
 		if err := d.serial.Start(); err != nil {
 			d.logger.Warnw("Failed to start first-time serial connection", "error", err)
+
+			// If the port is busy, that's because something else is connected - notify and quit
+			if errors.Is(err, os.ErrPermission) {
+				d.logger.Warnw("Serial port seems busy, notifying user and closing",
+					"comPort", d.config.ConnectionInfo.COMPort)
+
+				d.notifier.Notify(fmt.Sprintf("Can't connect to %s!", d.config.ConnectionInfo.COMPort),
+					"This serial port is busy, make sure to close any serial monitor or other deej instance.")
+
+				d.signalStop()
+
+				// also notify if the COM port they gave isn't found, maybe their config is wrong
+			} else if errors.Is(err, os.ErrNotExist) {
+				d.logger.Warnw("Provided COM port seems wrong, notifying user and closing",
+					"comPort", d.config.ConnectionInfo.COMPort)
+
+				d.notifier.Notify(fmt.Sprintf("Can't connect to %s!", d.config.ConnectionInfo.COMPort),
+					"This serial port doesn't exist, check your configuration and make sure it's set correctly.")
+
+				d.signalStop()
+			}
 		}
 	}()
 
