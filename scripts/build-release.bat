09680acb2e24d47befee7901c9781ea74136d836
omriharel <omriharel@users.noreply.github.com>
2020-05-02 15:55:25 +0300

    reduce executable size for release builds

--- scripts/build-release.bat
+++ scripts/build-release.bat
@@ -3,7 +3,7 @@
 IF "%GOPATH%"=="" GOTO NOGO
 
 ECHO Building deej (release)...
-go build -o deej-release.exe -ldflags -H=windowsgui .\cmd
+go build -o deej-release.exe -ldflags "-H=windowsgui -s -w"  .\cmd
 ECHO Done.
 GOTO DONE
 
