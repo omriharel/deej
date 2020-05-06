e36c0a6f508d312f0df008ca4650b45c4d4945be
medusalix <ss7@live.de>
2020-05-06 13:56:25 +0200

    Add Linux session support

--- session_finder_linux.go
+++ session_finder_linux.go
@@ -1,11 +1,115 @@
-// +build linux
-
 package deej
 
-import "go.uber.org/zap"
+import (
+	"fmt"
+	"net"
 
-func newSessionFinder(logger *zap.Logger) (SessionFinder, error) {
-	return nil, nil
+	"github.com/jfreymuth/pulse/proto"
+	"go.uber.org/zap"
+)
+
+type paSessionFinder struct {
+	logger        *zap.SugaredLogger
+	sessionLogger *zap.SugaredLogger
+
+	client *proto.Client
+	conn   net.Conn
 }
 
-// implement PulseAudio session finder here
+func newSessionFinder(logger *zap.SugaredLogger) (SessionFinder, error) {
+	client, conn, err := proto.Connect("")
+	if err != nil {
+		logger.Warnw("Failed to establish PulseAudio connection", "error", err)
+		return nil, fmt.Errorf("pulseaudio connect: %w", err)
+	}
+
+	request := proto.SetClientName{
+		Props: map[string]string{
+			"application.name": "deej",
+		},
+	}
+	reply := proto.SetClientNameReply{}
+
+	if err := client.Request(&request, &reply); err != nil {
+		return nil, err
+	}
+
+	sf := &paSessionFinder{
+		logger:        logger.Named("session_finder"),
+		sessionLogger: logger.Named("sessions"),
+		client:        client,
+		conn:          conn,
+	}
+
+	sf.logger.Debug("Created PA session finder instance")
+
+	return sf, nil
+}
+
+func (sf *paSessionFinder) GetAllSessions() ([]Session, error) {
+	sessions := []Session{}
+
+	// get the master session
+	master, err := sf.getMasterSession()
+	if err != nil {
+		sf.logger.Warnw("Failed to get master audio session", "error", err)
+		return nil, fmt.Errorf("get master audio session: %w", err)
+	}
+
+	sessions = append(sessions, master)
+
+	// enumerate sink inputs and add sessions along the way
+	if err := sf.enumerateAndAddSessions(&sessions); err != nil {
+		sf.logger.Warnw("Failed to enumerate audio sessions", "error", err)
+		return nil, fmt.Errorf("enumerate audio sessions: %w", err)
+	}
+
+	return sessions, nil
+}
+
+func (sf *paSessionFinder) getMasterSession() (Session, error) {
+	request := proto.GetSinkInfo{
+		SinkIndex: proto.Undefined,
+	}
+	reply := proto.GetSinkInfoReply{}
+
+	if err := sf.client.Request(&request, &reply); err != nil {
+		sf.logger.Warnw("Failed to get master sink info", "error", err)
+		return nil, fmt.Errorf("get master sink info: %w", err)
+	}
+
+	// create the master session
+	master := newMasterSession(sf.sessionLogger, sf.client, reply.SinkIndex, reply.Channels)
+
+	return master, nil
+}
+
+func (sf *paSessionFinder) enumerateAndAddSessions(sessions *[]Session) error {
+	request := proto.GetSinkInputInfoList{}
+	reply := proto.GetSinkInputInfoListReply{}
+
+	if err := sf.client.Request(&request, &reply); err != nil {
+		sf.logger.Warnw("Failed to get sink input list", "error", err)
+		return fmt.Errorf("get sink input list: %w", err)
+	}
+
+	for _, info := range reply {
+		name, ok := info.Properties["application.process.binary"]
+
+		if !ok {
+			sf.logger.Warnw("Failed to get sink input's process name",
+				"sinkInputIndex", info.SinkInputIndex)
+
+			continue
+		}
+
+		// create the deej session object
+		newSession := newPASession(sf.sessionLogger, sf.client, info.SinkInputIndex, info.Channels, name)
+
+		// add it to our slice
+		*sessions = append(*sessions, newSession)
+
+	}
+
+	return nil
+}
