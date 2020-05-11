4819a2a9f56b11278e6cfaa14abf1d0aac35b461
medusalix <ss7@live.de>
2020-05-11 13:12:23 +0200

    Make Windows scripts cwd-independent

--- scripts/windows/build-all.bat
+++ scripts/windows/build-all.bat
@@ -2,5 +2,8 @@
 
 ECHO Building deej (all)...
 
-CALL scripts\build-dev.bat
-CALL scripts\build-release.bat
+REM set windows scripts dir root in relation to script path to avoid cwd dependency
+SET "WIN_SCRIPTS_ROOT=%~dp0"
+
+CALL "%WIN_SCRIPTS_ROOT%build-dev.bat"
+CALL "%WIN_SCRIPTS_ROOT%build-release.bat"
