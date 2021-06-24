9504ba489a2c3cfaf59c39666bf7a103eb9528ef
Jack <bartnikjack@gmail.com>
2021-06-24 17:53:13 +0100

    added s and b to the serial output to diffrentiate between sliders and buttons

--- arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
+++ arduino/deej-5-sliders-vanilla/deej-5-sliders-vanilla.ino
@@ -1,11 +1,18 @@
 const int NUM_SLIDERS = 5;
 const int analogInputs[NUM_SLIDERS] = {A0, A1, A2, A3, A4};
+const int NUM_BUTTONS = 1;
+const int buttonInputs[NUM_BUTTONS] = {9};
 
 int analogSliderValues[NUM_SLIDERS];
+int buttonValues[NUM_BUTTONS];
 
-void setup() { 
+void setup() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
     pinMode(analogInputs[i], INPUT);
+  }
+
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    pinMode(buttonInputs[i], INPUT);
   }
 
   Serial.begin(9600);
@@ -14,13 +21,16 @@ void setup() {
 void loop() {
   updateSliderValues();
   sendSliderValues(); // Actually send data (all the time)
-  // printSliderValues(); // For debug
+//   printSliderValues(); // For debug
   delay(10);
 }
 
 void updateSliderValues() {
   for (int i = 0; i < NUM_SLIDERS; i++) {
-     analogSliderValues[i] = analogRead(analogInputs[i]);
+    analogSliderValues[i] = analogRead(analogInputs[i]);
+  }
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    buttonValues[i] = digitalRead(buttonInputs[i]);
   }
 }
 
@@ -28,13 +38,27 @@ void sendSliderValues() {
   String builtString = String("");
 
   for (int i = 0; i < NUM_SLIDERS; i++) {
+    builtString += "s";
     builtString += String((int)analogSliderValues[i]);
 
     if (i < NUM_SLIDERS - 1) {
       builtString += String("|");
     }
   }
-  
+
+  if(NUM_BUTTONS > 0){
+    builtString += String("|");
+  }
+
+  for (int i = 0; i < NUM_BUTTONS; i++) {
+    builtString += "b";
+    builtString += String((int)buttonValues[i]);
+
+    if (i < NUM_BUTTONS - 1) {
+      builtString += String("|");
+    }
+  }
+
   Serial.println(builtString);
 }
 
